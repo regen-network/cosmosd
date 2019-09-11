295074789e449147fe1e3572eefc428760ede6e3
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-09-11 15:46:27 +0200

    Test DoUpgrade without download

--- upgrade_test.go
+++ upgrade_test.go
@@ -40,9 +40,30 @@ func TestCurrentBin(t *testing.T) {
 func TestDoUpgrade(t *testing.T) {
 	home, err := copyTestData("validate")
 	require.NoError(t, err)
+	defer os.RemoveAll(home)
 
-	cfg := Config{Home: home, Name: "dummyd"}
+	cfg := &Config{Home: home, Name: "dummyd"}
 	assert.Equal(t, cfg.GenesisBin(), cfg.CurrentBin())
+
+	// do upgrade ignores bad files
+	for _, name := range []string{"missing", "nobin", "noexec"} {
+		info := &UpgradeInfo{Name: name}
+		err = DoUpgrade(cfg, info)
+		require.Error(t, err, name)
+		assert.Equal(t, cfg.GenesisBin(), cfg.CurrentBin(), name)
+	}
+
+	// make sure it updates a few times
+	for _, upgrade := range []string{"chain2", "chain3"} {
+		// now set it to a valid upgrade and make sure CurrentBin is now set properly
+		info := &UpgradeInfo{Name: upgrade}
+		err = DoUpgrade(cfg, info)
+		require.NoError(t, err)
+		// we should see current point to the new upgrade dir
+		assert.Equal(t, cfg.UpgradeBin(upgrade), cfg.CurrentBin())
+	}
+
+	// TODO: test with download
 }
 
 // copyTestData will make a tempdir and then
